2d12e1d1199f7150af765366a4522910185c8500
Maddie <maddiemcampbell@gmail.com>
2019-11-06 11:36:27 -0800

    import data.json, create express server, added routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 // import files and packages up here
-
+const morgan = require('morgan');
+const express = require('express');
+const topSpots = require('./data.json');
 
 // create your express server below
-var app;
+var app = express();
+app.use(morgan('dev'));
 
 // add your routes and middleware below
+app.get('/', function(req, res){
+    res.status(200).send('hello world!');
+});
+
+app.get('/data', function(req, res){
+    res.status(200).send(topSpots);
+});
 
 // finally export the express application
 module.exports = app;
